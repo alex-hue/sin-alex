4156fbf3ac4af9f4767aed7e3b1ce130e02ac667
alex-hue <55653789+alex-hue@users.noreply.github.com>
2019-09-29 20:20:19 +0200

    Add files via upload

---
+++ sin-alex/main.rb
@@ -0,0 +1,117 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'slim'
+require 'sass'
+require 'sinatra/flash'
+require 'pony'
+
+require './song'
+require './configTest'
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+helpers do
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+    end.join
+  end
+  def current?(path='/')
+    (request.path==path || request.path==path+'/') ? "current" : nil
+  end
+  def send_message
+    Pony.mail(
+      :from => params[:name] + "<" + params[:email] + ">",
+      :to => 'graffikkanv@gmail.com',
+      :subject => params[:name] + " has contacted you",
+      :body => params[:message],
+      :port => '587',
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.gmail.com',
+        :port => '587',
+        :enable_starttls_auto => true,
+        :user_name => 'graffikkanv@gmail.com',
+        :password => 'Sander7661',
+        :authentication => :plain,
+        :domain => 'localhost.localdomain'
+    })
+  end
+
+end
+
+def set_title
+  @title ||= "Songs By Sinatra"
+end
+
+before do
+  set_title
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  @title = "Home of Sinatra"
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  @title = "Contact us"
+  slim :contact
+end
+
+post '/contact' do
+  send_message
+  flash[:notice] = "Thank you for your message. We'll be in touch soon."
+  redirect to('/')
+end
+
+not_found do
+  @title = "Page Not Found"
+  slim :not_found
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    @loginname = params[:username]
+    flash[:notice] = "LOGGED IN successfully"
+    redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  flash[:notice] = "LOGGED OUT successfully"
+  redirect to('/')
+end
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  set :email_address => 'smtp.gmail.com',
+    :email_user_name => 'daz',
+    :email_password => 'secret',
+    :email_domain => 'localhost.localdomain'
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+  set :email_address => 'smtp.sendgrid.net',
+    :email_user_name => ENV['SENDGRID_USERNAME'],
+    :email_password => ENV['SENDGRID_PASSWORD'],
+    :email_domain => 'heroku.com'
+end
---
+++ sin-alex/song.rb
@@ -0,0 +1,85 @@
+require 'dm-core'
+require 'dm-migrations'
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+class Song
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :lyrics, Text
+  property :length, Integer
+  property :released_on, Date
+
+  def released_on=date
+    super Date.strptime(date, '%m/%d/%Y')
+  end
+end
+
+DataMapper.finalize
+
+module SongHelpers
+  def find_songs
+    @songs = Song.all
+  end
+
+  def find_song
+    Song.get(params[:id])
+  end
+
+  def create_song
+    @song = Song.create(params[:song])
+  end
+end
+
+helpers SongHelpers
+
+get '/get/hello' do
+  "Hello #{session[:name]}"
+end
+
+get '/songs' do
+  @title = "List of Songs changed 5"
+  find_songs
+  slim :songs
+end
+
+get '/songs/new' do
+  halt(401,'Not Authorized') unless session[:admin]
+  @title = "Add NEW Song"
+  @song = Song.new
+  slim :new_song
+end
+
+get '/songs/:id' do
+  @title = "Details Songs02"
+  @song = find_song
+  slim :show_song
+end
+
+get '/songs/:id/edit' do
+  @song = find_song
+  slim :edit_song
+end
+
+post '/songs' do
+  flash[:notice] = "Song successfully added" if create_song
+  redirect to("/songs/#{song.id}")
+end
+
+put '/songs/:id' do
+  song = find_song
+  if song.update(params[:song])
+    flash[:notice] = "Song successfully updated"
+  end
+  redirect to("/songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+  if find_song.destroy
+    flash[:notice] = "Song deleted"
+  end
+  redirect to('/songs')
+end
